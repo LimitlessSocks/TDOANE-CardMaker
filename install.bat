73657aa32de264fe916a8dbd0dbf9473b645ed08
LimitlessSocks <socktheringleader@gmail.com>
2023-07-17 15:21:06 -0400

    initialize ygoprodeck specific variant

--- install.bat
+++ install.bat
@@ -2,6 +2,8 @@
 ECHO Installing repository and submodules...
 git clone --recurse-submodules -j8 https://github.com/LimitlessSocks/TDOANE-CardMaker.git
 cd TDOANE-CardMaker
+REM branch specific: ygoprodeck variant
+git checkout ygoprodeck
 ECHO Instaling dependencies...
 npm install connect
 npm install serve-static
